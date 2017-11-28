d21e68258dcdb1d2eac03727e66f0a7fa53829a5
Emily Wise <wise.m.emily@gmail.com>
2017-11-28 15:26:25 -0700

    add failing test for show

--- spec/requests/api/v1/merchants/merchants_spec.rb
+++ spec/requests/api/v1/merchants/merchants_spec.rb
@@ -1,14 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe 'merchants API' do
+  let!(:merchants)   { create_list(:merchant, 10) }
+  let!(:merchant_id) { merchants.first.id }
+
   it "sends a list of merchants" do
-     create_list(:merchant, 10)
 
-      get '/api/v1/merchants'
+    get '/api/v1/merchants'
 
-      expect(response).to be_success
+    expect(response).to be_success
 
-      merchants = JSON.parse(response.body)
-      expect(merchants.count).to eq(10)
-   end
+    merchants = JSON.parse(response.body)
+    expect(merchants.count).to eq(10)
+    expect(json.first["name"]).to eq(merchants.first["name"])
+    expect(json.first["created_at"]).to be_nil
+    expect(json.first["updated_at"]).to be_nil
+  end
+
+  it "can get one merchant by its id" do
+
+    get "/api/v1/merchants/#{merchant_id}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["id"]).to eq(id)
+    expect(json['created_at']).to be_nil
+    expect(json['updated_at']).to be_nil
+  end
 end
