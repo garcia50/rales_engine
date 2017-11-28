248095f5232bac0f0a3c54d2a8ca49b1cce23e0d
Emily Wise <wise.m.emily@gmail.com>
2017-11-28 14:38:57 -0700

    add merchant controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
       resources :invoices, only: [:index, :show]
+      resources :merchants, only: [:index]
     end
   end
 end
