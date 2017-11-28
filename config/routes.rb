82d42f1a1e6c00903e8e275b5a42aa9dc375219f
Hugh Morrison <hmorri32@gmail.com>
2017-11-28 12:27:20 -0700

    alters merchant namespace to invoice namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     namespace :v1 do
-      namespace :merchants do
+      namespace :invoices do
         get 'find', to 'find#show'
       end
     end
