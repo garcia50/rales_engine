d1d3a002d6a0385ff95c065880e62a5a3ded2cd9
Emily Wise <wise.m.emily@gmail.com>
2017-11-28 15:52:12 -0700

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
       resources :invoices, only: [:index, :show]
       resources :merchants, only: [:index]
       resources :items,    only: [:index, :show]
+      resources :transactions,    only: [:index]
     end
   end
 end
--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :transaction do
-    credit_card_number {Faker::Number.number(16)}
-    credit_card_expiration_date {Faker::Date.between(2.days.ago, Date.today)}
+
     result { Faker::Hipster.word }
   end
 end
