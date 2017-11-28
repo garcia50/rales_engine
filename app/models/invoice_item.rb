55d55dbe3450d5c6bb189c2cb503f413656ae758
Hugh Morrison <hmorri32@gmail.com>
2017-11-28 15:23:47 -0700

    unit test validations and relationships invoice item

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,2 +1,5 @@
 class InvoiceItem < ApplicationRecord
+  validates  :quantity, :unit_price, presence: :true
+  belongs_to :invoice
+  belongs_to :item
 end
--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe InvoiceItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validations" do
+    it { should validate_presence_of(:quantity) }
+    it { should validate_presence_of(:unit_price) }
+  end
+
+  context "relationships" do
+    it { should belong_to(:invoice) }
+    it { should respond_to(:invoice) }
+    it { should belong_to(:item) }
+    it { should respond_to(:item) }
+  end
 end
