b30af509a1588f4a3d031e8607335d9bf5f63665
Emily Wise <wise.m.emily@gmail.com>
2017-11-28 16:48:57 -0700

    add failing test for customers

---
+++ spec/requests/api/v1/customers/customers_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe 'customers API' do
+  let!(:customers)   { create_list(:customer, 10) }
+  let!(:customer_id) { customers.first.id }
+
+  it "sends a list of customers" do
+
+    get '/api/v1/customers'
+
+    expect(response).to be_success
+    customers = JSON.parse(response.body)
+    expect(customers.count).to eq(10)
+    expect(json.first["first_name"]).to eq(customers.first["first_name"])
+    expect(json.first["last_name"]).to eq(customers.first["last_name"])
+    expect(json.first["created_at"]).to be_nil
+    expect(json.first["updated_at"]).to be_nil
+  end
+end  
