f0597abae8c85f9a81e1d00b26112cd85f85aa28
Emily Wise <wise.m.emily@gmail.com>
2017-11-28 17:34:53 -0700

    updated merchant test suite with relationships

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Merchant, type: :model do
     it {should validate_presence_of(:name)}
   end
 
+  context "relationsips" do
+    it { should have_many(:invoices) }
+    it { should have_many(:items) }
+  end
+
 end
