4bbd76025d47f9796e90a6f8b9dcd2f6165ee296
Hugh Morrison <hmorri32@gmail.com>
2017-11-29 16:57:14 -0700

    single finders done for invoice

--- spec/requests/api/v1/invoices/find_by_attribute_spec.rb
+++ spec/requests/api/v1/invoices/find_by_attribute_spec.rb
@@ -1,15 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe "user can find invoice by attribute in params", type: :request do
+  let!(:invoice) { create(:invoice) }
+
   context "single finders" do
     it "returns search based on the primary key" do
-      invoice = create(:invoice)
-
       get "/api/v1/invoices/find?id=#{invoice.id}"
 
       expect(json).to_not be_empty
       expect(json["id"]).to eq(invoice.id)
-      expect(json.length).to eq(1)
+    end
+
+    it 'returns json based on status' do
+      get "/api/v1/invoices/find?status=#{invoice.status}"
+
+      expect(json).to_not be_empty
+      expect(json["id"]).to eq(invoice.id)
+      expect(json["status"]).to eq(invoice.status)
+    end
+
+    it "returns a record based on created_at date" do
+      get "/api/v1/invoices/find?created_at=#{invoice.created_at}"
+
+      expect(json).to_not be_empty
+      expect(json["id"]).to eq(invoice.id)
+    end
+
+    it 'returns a recored based on updated_at date' do
+      get "/api/v1/invoices/find?updated_at=#{invoice.updated_at}"
+
+      expect(json).to_not be_empty
+      expect(json["id"]).to eq(invoice.id)
+    end
+
+    it "returns a record based on customer_id" do
+      get "/api/v1/invoices/find?customer_id=#{invoice.customer_id}"
+
+      expect(json).to_not be_empty
+      expect(json["id"]).to eq(invoice.id)
+    end
+
+    it "returns a record based on merchant_id" do
+      get "/api/v1/invoices/find?merchant_id=#{invoice.merchant_id}"
+
+      expect(json).to_not be_empty
+      expect(json["id"]).to eq(invoice.id)
     end
   end
 end
