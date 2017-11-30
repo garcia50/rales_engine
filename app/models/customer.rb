0d0e6e577787bbbaa4f7906c3c23fbe3a23bd033
Emily Wise <wise.m.emily@gmail.com>
2017-11-30 13:48:45 -0700

    passing favorite-customer-model-test

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,14 +6,14 @@ class Customer < ApplicationRecord
   has_many :transactions, through: :invoices
   has_many :merchants, through: :invoices
 
-  def favorite_merchant(limit=1)
+  def favorite_merchant
     merchants
     .select("merchants.*, count(invoices.customer_id) as number_of_invoices")
     .joins(invoices: [:transactions])
     .merge(Transaction.successful)
     .group('merchants.id')
     .order('number_of_invoices DESC')
-    .limit(limit)
+    .first
   end
 
 
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -10,4 +10,23 @@ RSpec.describe Customer do
     it { should have_many(:invoices) }
     it { should have_many(:transactions) }
   end
+
+  context "business analytics" do
+    describe "#favorite_merchant" do
+      it "returns a merchant where the customer the most successful transaction" do
+        customer = create :customer
+        merchant_1 = create :merchant
+        merchant_2 = create :merchant
+        invoice_1 = create :invoice, merchant: merchant_1, customer: customer
+        invoice_2 = create :invoice, merchant: merchant_1, customer: customer
+        invoice_3 = create :invoice, merchant: merchant_2, customer: customer
+
+        create(:transaction, result: 'success', invoice: invoice_1)
+        create(:transaction, result: 'success', invoice: invoice_2)
+        create(:transaction, result: 'success', invoice: invoice_3)
+
+        expect(customer.favorite_merchant).to eq(merchant_1)
+      end
+    end
+  end
 end
