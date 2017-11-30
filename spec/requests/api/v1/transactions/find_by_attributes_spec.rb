04387897ac2d60326c1a22ba0556610a3ac34323
Emily Wise <wise.m.emily@gmail.com>
2017-11-30 00:21:32 -0700

    wip transaction find

--- spec/requests/api/v1/transactions/find_by_attributes_spec.rb
+++ spec/requests/api/v1/transactions/find_by_attributes_spec.rb
@@ -54,59 +54,73 @@ RSpec.describe "user can find item by attribute in params", type: :request do
     expect(transaction["result"]).to eq(transactions.result)
   end
 
-  # it "returns all records based on id" do
-  #
-  #   transaction_1 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_2 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_3 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_4 = transaction.create!(name: "Zoey", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #
-  #   get "/api/v1/transactions/find_all?id=#{transaction_1.id}"
-  #
-  #   expect(response).to be_success
-  #   transactions = JSON.parse(response.body)
-  #   expect(transactions.class).to eq(Array)
-  # end
-  #
-  # it "returns all records based on name" do
-  #   transaction_1 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_2 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_3 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_4 = transaction.create!(name: "Zoey", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #
-  #   get "/api/v1/transactions/find_all?name=#{transaction_1.name}"
-  #
-  #   expect(response).to be_success
-  #   transactions = JSON.parse(response.body)
-  #   expect(transactions.class).to eq(Array)
-  #   expect(transactions.count).to eq(3)
-  # end
-  #
-  # it "returns all records based on created_at date" do
-  #   transaction_1 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_2 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_3 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_4 = transaction.create!(name: "Zoey", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #
-  #   get "/api/v1/transactions/find_all?created_at=#{transaction_1.created_at}"
-  #
-  #   expect(response).to be_success
-  #   transactions = JSON.parse(response.body)
-  #   expect(transactions.class).to eq(Array)
-  #   expect(transactions.count).to eq(4)
-  # end
-  #
-  # it "returns all records based on updated_at date" do
-  #   transaction_1 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_2 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_3 = transaction.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #   transaction_4 = transaction.create!(name: "Zoey", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-  #
-  #   get "/api/v1/transactions/find_all?updated_at=#{transaction_1.updated_at}"
-  #
-  #   expect(response).to be_success
-  #   transactions = JSON.parse(response.body)
-  #   expect(transactions.class).to eq(Array)
-  #   expect(transactions.count).to eq(4)
-  # end
+  it "returns all records based on id" do
+
+    transaction_1 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_2 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_3 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_4 = Transaction.create!(result: "failed", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    get "/api/v1/transactions/find_all?id=#{transaction_1.id}"
+
+    expect(response).to be_success
+    transactions = JSON.parse(response.body)
+    expect(transactions.class).to eq(Array)
+  end
+
+  it "returns all records based on result" do
+    transaction_1 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_2 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_3 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_4 = Transaction.create!(result: "failed", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    get "/api/v1/transactions/find_all?result=#{transaction_1.result}"
+
+    expect(response).to be_success
+    transactions = JSON.parse(response.body)
+    expect(transactions.class).to eq(Array)
+    expect(transactions.count).to eq(3)
+  end
+
+  it "returns all records based on invoice_id" do
+    transaction_1 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_2 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_3 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_4 = Transaction.create!(result: "failed", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    get "/api/v1/transactions/find_all?invoice_id=#{transaction_1.invoice_id}"
+
+    expect(response).to be_success
+    transactions = JSON.parse(response.body)
+    expect(transactions.class).to eq(Array)
+    expect(transactions.count).to eq(4)
+  end
+
+  it "returns all records based on created_at date" do
+    transaction_1 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_2 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_3 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_4 = Transaction.create!(result: "failed", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    get "/api/v1/transactions/find_all?created_at=#{transaction_1.created_at}"
+
+    expect(response).to be_success
+    transactions = JSON.parse(response.body)
+    expect(transactions.class).to eq(Array)
+    expect(transactions.count).to eq(4)
+  end
+
+  it "returns all records based on updated_at date" do
+    transaction_1 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_2 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_3 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_4 = Transaction.create!(result: "failed", invoice_id: 3, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    get "/api/v1/transactions/find_all?updated_at=#{transaction_1.updated_at}"
+
+    expect(response).to be_success
+    transactions = JSON.parse(response.body)
+    expect(transactions.class).to eq(Array)
+    expect(transactions.count).to eq(3)
+  end
 end
