5ebe23b6750dff20bc7aaa7781a75ee0ca275d22
Emily Wise <wise.m.emily@gmail.com>
2017-11-29 23:22:27 -0700

    added name parameter for find one

--- spec/requests/api/v1/merchants/find_by_attributes_spec.rb
+++ spec/requests/api/v1/merchants/find_by_attributes_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe "user can find item by attribute in params", type: :request do
     expect(merchant["created_at"]).to be_nil
     expect(merchant["updated_at"]).to be_nil
   end
+
+  it "returns a single record based on name" do
+    get "/api/v1/merchants/find?name=#{merchants.name}"
+
+    expect(response).to be_success
+
+    merchant = JSON.parse(response.body)
+    expect(merchant["name"]).to eq(merchants.name)
+    expect(merchant["created_at"]).to be_nil
+    expect(merchant["updated_at"]).to be_nil
+  end
 end
