38394f4af014d9cd130d70608df26ed9aef868fe
Hugh Morrison <hmorri32@gmail.com>
2017-11-30 10:40:03 -0700

    model test complete most_revenue(x)

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ApplicationRecord
   belongs_to :merchant
   has_many   :invoice_items
   has_many   :invoices, through: :invoice_items
+
+  def self.most_revenue(x)
+    joins(invoice_items: [invoice: :transactions])
+      .merge(Transaction.successful)
+      .group('items.id')
+      .order('sum(invoice_items.quantity * invoice_items.unit_price) DESC')
+      .take(x)
+  end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,4 +11,20 @@ RSpec.describe Item, type: :model do
     it { should have_many(:invoices).through(:invoice_items) }
     it { should belong_to(:merchant) }
   end
+
+  context "business analytics" do
+    describe "self.most_revenue" do
+      it "returns the top x items ranked by total revenue generated" do
+        cool_item, item_2 = create_list(:item, 2)
+        invoice = create(:invoice)
+
+        create(:invoice_item, item:    cool_item, invoice: invoice, unit_price: 1000)
+        create(:invoice_item, item:    item_2,    invoice: invoice, unit_price: 1)
+        create(:transaction,  invoice: invoice,   result:  "success")
+
+        expect(Item.most_revenue(2).first).to eq(cool_item)
+        expect(Item.most_revenue(2).last).to eq(item_2)
+      end
+    end
+  end
 end
