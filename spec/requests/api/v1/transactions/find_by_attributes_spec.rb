b82e74f79fcb266e01dca35d623c6d60e0f6f880
Emily Wise <wise.m.emily@gmail.com>
2017-11-30 09:47:51 -0700

    passing transactions tests

--- spec/requests/api/v1/transactions/find_by_attributes_spec.rb
+++ spec/requests/api/v1/transactions/find_by_attributes_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe "user can find item by attribute in params", type: :request do
   let!(:transactions) { create(:transaction) }
+  let!(:invoice) { create(:invoice) }
+  let!(:invoice_1) { create(:invoice) }
 
   it "returns a single record based on id" do
     get "/api/v1/transactions/find?id=#{transactions.id}"
@@ -56,10 +58,11 @@ RSpec.describe "user can find item by attribute in params", type: :request do
 
   it "returns all records based on id" do
 
-    transaction_1 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_2 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_3 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_4 = Transaction.create!(result: "failed", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    transaction_1 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_2 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_3 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_4 = Transaction.create!(result: "failed", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
 
     get "/api/v1/transactions/find_all?id=#{transaction_1.id}"
 
@@ -69,10 +72,10 @@ RSpec.describe "user can find item by attribute in params", type: :request do
   end
 
   it "returns all records based on result" do
-    transaction_1 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_2 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_3 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_4 = Transaction.create!(result: "failed", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_1 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_2 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_3 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_4 = Transaction.create!(result: "failed", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
 
     get "/api/v1/transactions/find_all?result=#{transaction_1.result}"
 
@@ -83,10 +86,10 @@ RSpec.describe "user can find item by attribute in params", type: :request do
   end
 
   it "returns all records based on invoice_id" do
-    transaction_1 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_2 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_3 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_4 = Transaction.create!(result: "failed", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_1 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_2 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_3 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_4 = Transaction.create!(result: "failed", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
 
     get "/api/v1/transactions/find_all?invoice_id=#{transaction_1.invoice_id}"
 
@@ -97,10 +100,10 @@ RSpec.describe "user can find item by attribute in params", type: :request do
   end
 
   it "returns all records based on created_at date" do
-    transaction_1 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_2 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_3 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_4 = Transaction.create!(result: "failed", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_1 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_2 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_3 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_4 = Transaction.create!(result: "failed", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
 
     get "/api/v1/transactions/find_all?created_at=#{transaction_1.created_at}"
 
@@ -111,10 +114,10 @@ RSpec.describe "user can find item by attribute in params", type: :request do
   end
 
   it "returns all records based on updated_at date" do
-    transaction_1 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_2 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_3 = Transaction.create!(result: "succes", invoice_id: 1, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
-    transaction_4 = Transaction.create!(result: "failed", invoice_id: 3, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_1 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_2 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_3 = Transaction.create!(result: "succes", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    transaction_4 = Transaction.create!(result: "failed", invoice_id: invoice.id, created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-06-27T14:54:05.000Z")
 
     get "/api/v1/transactions/find_all?updated_at=#{transaction_1.updated_at}"
 
