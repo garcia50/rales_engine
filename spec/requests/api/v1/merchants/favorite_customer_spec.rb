3c0bf2b30a12e07056db911e098eb924b4075743
Hugh Morrison <hmorri32@gmail.com>
2017-11-30 09:26:31 -0700

    renames BI to business_analytics

--- spec/requests/api/v1/merchants/favorite_customer_spec.rb
+++ spec/requests/api/v1/merchants/favorite_customer_spec.rb
@@ -5,18 +5,48 @@ RSpec.describe "Merchant favorite customer API", type: :response do
     it "returns a favorite customer for a merchant based on successful transactions" do
       merchant = double()
       customer = double()
+      get '/api/v1/merchants/1/favorite_customer'
 
-      allow(Merchant).to receive()
+      # allow(Merchant).to receive()
 
     end
   end
 end
 
 
-describe "get /api/v1/merchants/:id/revenue" do
-  it "calls revenue on the merchant specified in the params and returns revenue as json" do
+# describe "get /api/v1/merchants/:id/revenue" do
+#   it "calls revenue on the merchant specified in the params and returns revenue as json" do
     
-    # merchant = double()
-  end
-end
+#     expect(response).to be_success
+#     # merchant = double()
+#     # allow(Merchant).to receive(:find).with("1") { merchant }
+#     # expect(merchant).to receive(:revenue).once { 700.12 }
 
+#     # get "/api/v1/merchants/1/revenue"
+
+#     # revenue = JSON.parse(response.body, symbolize_names: true)
+
+#     # expect(response).to be_success
+#     # expect(revenue).to have_key(:revenue)
+#     # expect(revenue[:revenue]).to eq("700.12")
+#   end
+# end
+
+# it "returns the customer who has conducted the most total number of successful transactions" do
+#   merchant = create(:merchant)
+#   customer_1 = create(:customer)
+#   customer_2 = create(:customer)
+#   customer_1_invoices = create_list(:invoice, 2, customer_id: customer_1.id, merchant_id: merchant.id)
+#   customer_2_invoice = create(:invoice, customer_id: customer_2.id, merchant_id: merchant.id)
+#   create(:transaction, invoice_id: customer_2_invoice.id, result: "success")
+#   customer_1_invoices.each do |invoice|
+#     create(:transaction, invoice_id: invoice.id, result: "success")
+#   end
+
+#   get "/api/v1/merchants/#{merchant.id}/favorite_customer"
+#   customer = JSON.parse(response.body)
+
+#   expect(response).to be_success
+#   expect(customer).to be_instance_of(Hash)
+#   expect(customer["id"]).to eq(customer_1.id)
+# end
