8eb1b5602086bad0ab954fab91adc2533c489ece
Emily Wise <wise.m.emily@gmail.com>
2017-11-29 22:12:26 -0700

    added test for item and invoice_items endpoint

---
+++ spec/requests/api/v1/relationships/items_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Item relationships API", type: :request do
+  let!(:items)   { create(:item) }
+  let!(:item_id) { items.id }
+  let!(:invoice) {create(:invoice)}
+  let!(:invoice_items_1) { InvoiceItem.create!(quantity: 4, unit_price: 33, item: items, invoice: invoice) }
+  let!(:invoice_items_2) { InvoiceItem.create!(quantity: 5, unit_price: 33, item: items, invoice: invoice )}
+
+  it 'returns all of a associated invoice_items' do
+    get "/api/v1/items/#{item_id}/invoice_items"
+
+      expect(response).to be_success
+
+      invoice_items = JSON.parse(response.body)
+      expect(transactions.count).to eq(2)
+      expect(invoice_items.first["quantity"]).to eq(invoice_items_1.quantity)
+   end
+end
+
+
+
+# GET /api/v1/items/:id/merchant returns the associated merchant
