6562d62ee51a6075c5a68b747adb7af9bf38faca
Hugh Morrison <hmorri32@gmail.com>
2017-11-30 14:33:56 -0700

    duplicate in customers transatcions

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,6 @@ Rails.application.routes.draw do
       namespace :customers do
         get "/:id/invoices",          to: 'invoices#index'
         get "/:id/transactions",      to: 'transactions#index'
-        get "/:id/transactions",      to: 'transactions#index'
         get "/:id/favorite_merchant", to: 'favorite_merchant#show'
       end
 
