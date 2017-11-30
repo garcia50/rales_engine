e1dc44f723badcbc7f7bfea8e83f0749dbce08b1
Hugh Morrison <hmorri32@gmail.com>
2017-11-30 09:05:59 -0700

    defines more relationships on model merchant and tests. defines favorite customer spec for merchant;

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
   context "validations" do
-    it {should validate_presence_of(:name)}
+    it { should validate_presence_of(:name) }
   end
 
   context "relationsips" do
