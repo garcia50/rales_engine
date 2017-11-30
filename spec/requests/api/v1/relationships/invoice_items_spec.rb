9166a3872620e66bbb77f1efdcbf4c38eeb4efb8
Emily Wise <wise.m.emily@gmail.com>
2017-11-29 22:47:06 -0700

    wip invoice_item relationship

---
+++ spec/requests/api/v1/relationships/invoice_items_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "Invoice_items relationships API", type: :request do
+  let!(:invoice) {create(:invoice)}
+  let!(:item) { create(:item)}
+  let!(:invoice_items_1) { InvoiceItem.create!(quantity: 4, unit_price: 33, item: item, invoice: invoice) }
+  let!(:invoice_item_id) { invoice_items_1.id }
+
+  it 'returns associated invoice' do
+    get "/api/v1/invoice_items/#{invoice_item_id}/invoice"
+
+      expect(response).to be_success
+
+      invoice = JSON.parse(response.body)
+
+      expect(invoice["status"]).to eq(invoice_items_1.invoice.status)
+   end
+end
+
+
+# GET /api/v1/invoice_items/:id/item returns the associated item
