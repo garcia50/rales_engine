8c417c4b47d73f4803fc4f44ef2fb7cf833827f4
Hugh Morrison <hmorri32@gmail.com>
2017-11-30 09:04:22 -0700

    defines more relationships on model merchant and tests. defines favorite customer spec for merchant;

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,7 +1,13 @@
 class Merchant < ApplicationRecord
   validates :name, presence: :true
-  has_many :items
-  has_many :invoices
+
+  has_many  :items
+  has_many  :invoices
+
+  has_many  :customers,     through: :invoices
+  has_many  :invoice_items, through: :invoices
+  has_many  :transactions,  through: :invoices
+
 
   def total_revenue(date = nil)
     if date
@@ -13,11 +19,20 @@ class Merchant < ApplicationRecord
       invoices.joins(:transactions, :invoice_items)
       .merge(Transaction.successful)
       .sum('invoice_items.unit_price * invoice_items.quantity')
-    end  
+    end
   end
 
   def total_revenue_response
     formatted_revenue = total_revenue/100.0
     {revenue: formatted_revenue}
+  end
+
+  def favorite_customer
+    customers
+      .joins(invoices: [:transactions])
+      .merge(Transaction.successful)
+      .group('customers.id')
+      .order('count(invoices.*) desc')
+      .first
   end
 end
--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -8,6 +8,27 @@ RSpec.describe Merchant, type: :model do
   context "relationsips" do
     it { should have_many(:invoices) }
     it { should have_many(:items) }
+    it { should have_many(:customers).through(:invoices) }
+    it { should have_many(:invoice_items).through(:invoices) }
+    it { should have_many(:transactions).through(:invoices) }
+
   end
 
+  context "business analytics" do
+    describe "#favorite_customer" do
+      it "loads favorite customer associated with one merchant" do
+        merchant      = create :merchant
+        cool_customer = create :customer
+        weak_customer = create :customer
+        invoice_1     = create :invoice, merchant: merchant, customer: cool_customer
+        invoice_2     = create :invoice, merchant: merchant, customer: cool_customer
+        invoice_3     = create :invoice, merchant: merchant, customer: weak_customer
+
+        create_list(:transaction, 4, result: 'success', invoice: invoice_1)
+        create_list(:transaction, 2, result: 'success', invoice: invoice_3)
+
+        expect(merchant.favorite_customer).to eq(cool_customer)
+      end
+    end
+  end
 end
