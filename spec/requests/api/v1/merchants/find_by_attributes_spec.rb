6603d1fc1556deec00d0e4304c90aa3e4e465efc
Emily Wise <wise.m.emily@gmail.com>
2017-11-29 23:51:43 -0700

    added find and find_all for merchants

--- spec/requests/api/v1/merchants/find_by_attributes_spec.rb
+++ spec/requests/api/v1/merchants/find_by_attributes_spec.rb
@@ -42,4 +42,60 @@ RSpec.describe "user can find item by attribute in params", type: :request do
     merchant = JSON.parse(response.body)
     expect(merchant["name"]).to eq(merchants.name)
   end
+
+  it "returns all records based on id" do
+
+    merchant_1 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_2 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_3 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_4 = Merchant.create!(name: "Zoey", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    get "/api/v1/merchants/find_all?id=#{merchant_1.id}"
+
+    expect(response).to be_success
+    merchants = JSON.parse(response.body)
+    expect(merchants.class).to eq(Array)
+  end
+
+  it "returns all records based on name" do
+    merchant_1 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_2 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_3 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_4 = Merchant.create!(name: "Zoey", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    get "/api/v1/merchants/find_all?name=#{merchant_1.name}"
+
+    expect(response).to be_success
+    merchants = JSON.parse(response.body)
+    expect(merchants.class).to eq(Array)
+    expect(merchants.count).to eq(3)
+  end
+
+  it "returns all records based on created_at date" do
+    merchant_1 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_2 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_3 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_4 = Merchant.create!(name: "Zoey", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    get "/api/v1/merchants/find_all?created_at=#{merchant_1.created_at}"
+
+    expect(response).to be_success
+    merchants = JSON.parse(response.body)
+    expect(merchants.class).to eq(Array)
+    expect(merchants.count).to eq(4)
+  end
+
+  it "returns all records based on updated_at date" do
+    merchant_1 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_2 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_3 = Merchant.create!(name: "Emily", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+    merchant_4 = Merchant.create!(name: "Zoey", created_at: "2012-03-27T14:54:05.000Z", updated_at: "2012-03-27T14:54:05.000Z")
+
+    get "/api/v1/merchants/find_all?updated_at=#{merchant_1.updated_at}"
+
+    expect(response).to be_success
+    merchants = JSON.parse(response.body)
+    expect(merchants.class).to eq(Array)
+    expect(merchants.count).to eq(4)
+  end
 end
