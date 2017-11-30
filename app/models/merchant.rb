1a291cf8f56cd23ef151c004d8ed0784c4bfbc8e
Emily Wise <wise.m.emily@gmail.com>
2017-11-29 21:02:30 -0700

    updated transcation and merchant model for revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,15 @@ class Merchant < ApplicationRecord
   validates :name, presence: :true
   has_many :items
   has_many :invoices
+
+  def total_revenue
+    invoices.joins(:transactions, :invoice_items)
+    .merge(Transaction.successful)
+    .sum('invoice_items.unit_price * invoice_items.quantity')
+  end
+
+  def total_revenue_response
+    formatted_revenue = total_revenue/100.0
+    {revenue: formatted_revenue}
+  end  
 end
