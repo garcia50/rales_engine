59bce12ce4e96dc41bc0266479bccb43cffd047c
Hugh Morrison <hmorri32@gmail.com>
2017-12-01 07:10:09 -0700

    cleans up invoiceitemn.rb

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -2,20 +2,4 @@ class InvoiceItem < ApplicationRecord
   validates  :quantity, :unit_price, presence: :true
   belongs_to :invoice
   belongs_to :item
-
-  def self.delegate_single_finder(params, valid_params)
-    if params["unit_price"]
-      InvoiceItem.find_by(unit_price: (params["unit_price"].to_f * 100).round(2))
-    else
-      InvoiceItem.find_by(valid_params)
-    end
-  end
-
-  def self.delegate_multiple_finder(params, valid_params)
-    if params["unit_price"]
-      InvoiceItem.where(unit_price: (params["unit_price"].to_f * 100).round(2))
-    else
-      InvoiceItem.where(valid_params)
-    end
-  end
 end
