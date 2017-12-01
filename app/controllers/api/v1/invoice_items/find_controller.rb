4804c7ca783d6b225b738677769bc55c2fe83756
Hugh Morrison <hmorri32@gmail.com>
2017-11-30 21:27:52 -0700

    resolves unknown format param error on invoice items find controller

--- app/controllers/api/v1/invoice_items/find_controller.rb
+++ app/controllers/api/v1/invoice_items/find_controller.rb
@@ -4,18 +4,13 @@ class Api::V1::InvoiceItems::FindController < ApplicationController
   end
 
   def show
+
     json_response(InvoiceItem.find_by(valid_params))
   end
 
   private
 
   def valid_params
-    params.permit(:id,
-                  :invoice_id,
-                  :item_id,
-                  :quantity,
-                  :unit_price,
-                  :created_at,
-                  :updated_at)
+    params.permit(:id, :invoice_id, :item_id, :quantity, :unit_price, :created_at, :updated_at).permit(:format)
   end
 end
